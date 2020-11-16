ba3864d6104b5022b659c72a05357cfba25e0d68
Anton <mironov.altair@gmail.com>
2020-11-16 10:56:05 +0300

    create app poll

---
+++ poll/models.py
@@ -0,0 +1,17 @@
+import datetime
+from django.db import models
+
+
+class Poll(models.Model):
+    question = models.TextField()
+    date_published = models.DateTimeField(verbose_name = "Дата публикации", default = datetime.datetime.now())
+    option_one = models.CharField(max_length=30)
+    option_two = models.CharField(max_length=30)
+    option_three = models.CharField(max_length=30)
+    option_one_count= models.IntegerField(default=0)
+    option_two_count= models.IntegerField(default=0)
+    option_three_count= models.IntegerField(default=0)
+
+    
+    def total(self):
+        return self.option_one_count + self.option_two_count + self.option_three_count
